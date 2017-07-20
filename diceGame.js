77650bab7d10f34cb30c42add8329c1023231d8e
camschoch <camschoch3@gmail.com>
2017-07-20 12:00:21 -0500

    added functions for player info

--- diceGame.js
+++ diceGame.js
@@ -2,42 +2,50 @@
 
 function startProgram(){
 
-	function setFourSidedDie(){
-		var fourSidedDie = [1, 2, 3, 4];
-		var randomFourSidedDie = fourSidedDie[Math.floor(Math.random() * fourSidedDie.length)];
-		return randomFourSidedDie;
+	function getPlayerInfo(){
+		var testArray = []
+		var firstname = prompt("please enter first name here.");
+		testArray.push(firstname);
+		var lastName = prompt("Now enter your last name.");
+		testArray.push(lastName);
+		return testArray
 	}
 
-	function setSixSidedDie(){
-		var sixSidedDie = [1, 2, 3, 4, 5, 6];
-		var randomSixSidedDie = sixSidedDie[Math.floor(Math.random() * sixSidedDie.length)];
-		return randomSixSidedDie;
+	//function getPlayerTwoInfo(){}
+
+	function testDiceArray(){
+		var diceArray = [4, 6, 8, 10, 12, 20];
+		return diceArray;
 	}
 
-	function setEightSidedDie(){
-		var eightSidedDie = [1, 2, 3, 4, 5, 6, 7, 8];
-		var randomEightSidedDie = eightSidedDie[Math.floor(Math.random() * eightSidedDie.length)];
-		return randomEightSidedDie;
+	function chooseRandomDie(diceArray){
+		var randomDie = diceArray[Math.floor(Math.random() * diceArray.length)];
+		return randomDie;
 	}
 
-	function setTenSidedDie(){
-		var tenSidedDie = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-		var randomTenSidedDie = tenSidedDie[Math.floor(Math.random() * tenSidedDie.length)];
-		return randomTenSidedDie;
+	function getRolledRandomDie(randomDie){
+		var rollRandomDie = Math.round(Math.random() * randomDie);
+			if(rollRandomDie !== 0){
+				console.log(rollRandomDie);
+				return rollRandomDie;
+			}
+		getRolledRandomDie(randomDie);
+		
 	}
 
-	function setTwelveSidedDie(){
-		var twelveSidedDie = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-		var randomTwelveSidedDie = twelveSidedDie[Math.floor(Math.random() * twelveSidedDie.length)];
-		return randomTwelveSidedDie;
+
+	function setPlayerInfo(rollRandomDie, testArray){
+		var players = [
+			{
+				"playerFirstName": testArray.pop([0]);
+				"playerLastName": testArray.pop([1]);
+			
+			}
 	}
 
-	function setTwentySidedDie(){
-		var twentySidedDie = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
-		var randomTwentySidedDie = twentySidedDie[Math.floor(Math.random() * twentySidedDie.length)];
-		return randomTwentySidedDie;
-	}
-
+	var diceArray = testDiceArray();
+	var randomDie = chooseRandomDie(diceArray);
+	var rollRandomDie = getRolledRandomDie(randomDie); 
 
 }
 
